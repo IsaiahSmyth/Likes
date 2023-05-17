d71d109bd16586f50073652b461feefa24b5313d
punchme2 <smyth.isaiah@gmail.com>
2023-05-16 20:44:16 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function count(){
+    var counting = document.getElementById("counter1").innerHTML;
+    counting++;
+    document.getElementById("counter1").innerHTML = counting;
+};
+
+function count2(){
+    var counting = document.getElementById("counter2").innerHTML;
+    counting++;
+    document.getElementById("counter2").innerHTML = counting;
+};
+
+
+function count3(){
+    var counting = document.getElementById("counter3").innerHTML;
+    counting++;
+    document.getElementById("counter3").innerHTML = counting;
+};
+
+
+
